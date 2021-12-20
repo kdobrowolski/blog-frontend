605e444a4612143c29894975fdb4fcfa0d8069ba
kdobrowolski <kdobrowolski12345@gmail.com>
2021-12-20 22:09:54 +0100

    Added tiptap-vuetify to Editor component

---
+++ pages/admin/posts/_post.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Admin_post_page">
+    <h1 class="page_header">Panel administratora</h1>
+    <Button value="Usuń post" />
+    <Editor />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+import Editor from '~/components/Editor.vue';
+import Button from '~/components/Button.vue';
+import '~/assets/scss/pages/admin/posts/_post.scss';
+
+@Component({
+  components: {
+    Button,
+    Editor
+  }
+})
+export default class AdminPostPage extends Vue {
+
+}
+</script>
