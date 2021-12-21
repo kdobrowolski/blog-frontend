b53d1614c922fd48c5f2c930e33b5928df23f4f1
kdobrowolski <kdobrowolski12345@gmail.com>
2021-12-21 20:33:15 +0100

    Created /admin/moderators, /admin/moderators/add and /admin/moderators/_moderator pages

---
+++ pages/admin/moderators/_moderator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="Admin_moderators_add_page">
+    <h1 class="page_header">
+      Panel administratora
+    </h1>
+    <ModeratorForm moderator-exist />
+    <Button value="Usuń moderatora" />
+    <Button value="Wróć" href="/admin/moderators" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+import Button from '~/components/Button.vue';
+import ModeratorForm from '~/components/ModeratorForm.vue';
+import '~/assets/scss/pages/admin/moderators/add.scss';
+
+@Component({
+  components: {
+    Button,
+    ModeratorForm
+  }
+})
+export default class AdminModeratorPage extends Vue {
+
+}
+</script>
